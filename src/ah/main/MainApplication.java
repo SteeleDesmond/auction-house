802648c3b5791eca97c6c50d907d4f1bc2cae09b
gavin <gavinmcguire95@gmail.com>
2018-11-15 10:58:10 -0700

    update javafx application/git test

--- src/ah/main/MainApplication.java
+++ src/ah/main/MainApplication.java
@@ -1,7 +1,18 @@
 package ah.main;
 
-public class MainApplication {
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class MainApplication extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+
     public static void main(String[] args) {
+
 
     }
 }
