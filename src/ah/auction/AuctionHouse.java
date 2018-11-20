d4283724386e0b4607485ead8f164a068ac5ee63
samiddaugh <smiddaugh@unm.edu>
2018-11-20 11:13:07 -0700

    randomly picking set number of items complete;

--- src/ah/auction/AuctionHouse.java
+++ src/ah/auction/AuctionHouse.java
@@ -3,22 +3,24 @@ package ah.auction;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.util.LinkedList;
+import java.util.Random;
 import java.util.Scanner;
 import ah.shared.Item;
 
 
 public class AuctionHouse {
     private LinkedList<Item> itemList;
+    private int numberOfItems = 5; //for sale, pick from temp list
+    private Random rand = new Random(); //temp seed
 
     public AuctionHouse(){
         itemList = new LinkedList<Item>();
     }
 
     public static void main(String[] args){
-        System.out.println("In auction house.");
+        //System.out.println("In auction house.");
         AuctionHouse aHouse = new AuctionHouse();
-        Scanner commandLine = new Scanner(System.in);
-        //System.out.println("Read in file__:");
+       // Scanner commandLine = new Scanner(System.in);
         //String command = commandLine.nextLine();
         aHouse.readItemList("resrcs/itemList.txt");
         System.out.println("Printing item list:");
@@ -27,18 +29,33 @@ public class AuctionHouse {
 
 
     private boolean readItemList(String fileName){
+        LinkedList<Item> items = new LinkedList<>();
         try{
             Scanner readin = new Scanner(new FileReader(fileName));
             //Note adds ALL things in text file to auction house  itemlist
             while(readin.hasNextLine()){
                 String input = readin.nextLine();
                 Item item = new Item(input);
-                itemList.add(item);
+                items.add(item);
             }
             readin.close();
         }catch (FileNotFoundException ex){
             System.out.println("file not found");
             return false;
+        }
+        //need to randomly pick a certain number of items
+        while(numberOfItems>0){
+            int next = rand.nextInt(items.size());
+            itemList.add(items.remove(next));
+            numberOfItems--;
+        }
+        System.out.println("List of items read in");
+        if(!items.isEmpty()) {
+            for (Item i : items) {
+                System.out.println(i);
+            }
+        }else{
+            System.out.println("nothing");
         }
 
         return true;
