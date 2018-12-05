82387ccba9d59d931695b4bbb10007f999e845b2
Savanna Middaugh <smiddaugh@unm.edu>
2018-12-05 12:44:32 -0700

    add two classes to help with keeping auction house inventories

---
+++ src/ah/auction/Inventory.java
@@ -0,0 +1,17 @@
+package ah.auction;
+
+import java.util.LinkedList;
+
+public class Inventory {
+    private LinkedList<ItemInformation> inStock;
+
+    public Inventory(){
+        inStock = new LinkedList<>();
+    }
+
+    public void addToInventory(Item item, int minBid){
+
+    }
+
+    //use this as an inventory for the auction house. Move inventory stuff here...
+}
