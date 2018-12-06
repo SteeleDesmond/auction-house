61db5b742073c2d3db71e86d85d13ce0bc4eac0e
Savanna Middaugh <smiddaugh@unm.edu>
2018-12-05 17:03:30 -0700

    removed extranious classes

--- src/ah/auction/ItemInformation.java
+++
@@ -1,9 +0,0 @@
-package ah.auction;
-
-public class ItemInformation {
-    private int IDNum;
-    private Item item;
-    private int minBid;
-    private int CurrBid;
-    //wait, why not add these fields to id?
-}
