8eb9c1290e49b59b62071cb8be3a83eba63e7154
Savanna Middaugh <smiddaugh@unm.edu>
2018-12-05 22:38:01 -0700

    cleaned up auction house code

--- src/ah/auction/AuctionHouse.java
+++ src/ah/auction/AuctionHouse.java
@@ -13,17 +13,12 @@ public class AuctionHouse {
     private String name;
     private String bankID; //keep for now
     private LinkedList<Item> inventory;
-    private int defaultItemNum = 5;//make this a local variable
-    private int numberOfItems = 5; //for sale, pick from temp list //keep
-    private int maxMinBid = 1000; //this is the maximum that the minimum bid can be
-    private Random rand = new Random();
-    private boolean quit = false; //may not need
+    private int defaultItemNum = 5;//easy access
+    private int numberOfItems = 5; //...don't need to change this
+    private int maxMinBid = 1000; //easy access
+    private Random rand = new Random(); //for all the random needs
 
     private LinkedList<Bid> activeAuctions;  //may not need
-    private LinkedList<Item> pendingAuctions; //items people want, but cannot //remove
-    //bid for yet
-    //probably need a way to keep track of bidders....
-    private LinkedList bidders; //may not need, espically since i keep track in item the person who is doing it
 
     /**
      * creates an Auction house object
@@ -51,19 +46,19 @@ public class AuctionHouse {
         readItemList("resrcs/itemList.txt");
         System.out.println("Complete.");
 
-        //bank related things set after creation
-
         System.out.println("Auction House is now operational");
+
+        //bank related things set after creation, or before...just not here
     }
 
+    /**
+     * temporary main, used for testing right now.
+     * @param args not used
+     */
     public static void main(String[] args){
-        //System.out.println("In auction house.");
-        System.out.println("Welcome.");
         Scanner commandLine = new Scanner(System.in);
         AuctionHouse ah = new AuctionHouse(commandLine);
         System.out.println(ah.getInventoryList());
-        //ah.startAuction(new Item("excalibur"),400);
-       // System.out.println(ah.activeAuctions);
         Item item = ah.inventory.getFirst();
         Item item2 = ah.inventory.getLast();
         ah.makeBid(item,30,"bob");
@@ -73,84 +68,20 @@ public class AuctionHouse {
         ah.makeBid(item,1001,"darrel");
         System.out.println(ah.getInventoryList());
 
-//        System.out.println("For commands, enter help");
-//        while(!aHouse.quit){
-//            command = commandLine.nextLine();
-//            switch(command){
-//                case("quit"):
-//                    aHouse.quit = true;
-//                    break;
-//                case("help"):
-//                    aHouse.printHelp();
-//                    break;
-//                case("print items"):
-//                    System.out.println("Printing item list:");
-//                    aHouse.printItemList();
-//                    break;
-//                case("bank"):
-//                    System.out.println("Set up bank account here");
-//                    //need to create the controller here, and the server
-//                    //in order: server, bank, controller
-//                    break;
-//                case("server"):
-//                    System.out.println("create server here");
-//                    break;
-//                case("Start auction"):
-//                    System.out.println("start an acution for an item");
-//                    break;
-//                case("view interest"):
-//                    System.out.println("view flagged items");
-//
-//                    break;
-//                case("view auctions"):
-//                    System.out.println("view active auctions");
-//                    aHouse.printActiveAuctions();
-//                    break;
-//                case ("t"):
-//                    System.out.println("create bid");
-//                    Item item = new Item("hell");
-//                    Bid bid = new Bid(item, 30,"bob");
-//                    System.out.println(bid);
-//                    break;
-//                case("add item"):
-//                    aHouse.addCustomItem();
-//                    break;
-
-//
-//            }
-
-            //probs should check notification here
-
-
-//        }
-
-        //aHouse.getServerInfoFromUser();
-
-
-
         commandLine.close();
     }
 
     /**
      * Gets the list of items the auction is selling, all of them
-     * @return a string representation of that
+     * @return a string representation of that information
      */
-    public String getInventoryList(){ /**need to fix this**/
+    public String getInventoryList(){
         String ret = name+"\n";
         for(Item i: inventory){
             ret = ret+i.toString();
             ret = ret+"\n";
         }
         return ret;
-    }
-
-    public void startAuction(Item item, int minMoney){ //def need to fix/remove this
-        Item mcGuffin = findItemInInventory(item);
-        if(mcGuffin==null){
-            return;
-        }
-        Bid minBid = new Bid(mcGuffin,minMoney, null);
-        activeAuctions.add(minBid);
     }
 
     /**
@@ -167,47 +98,35 @@ public class AuctionHouse {
      * started, or, if the bid is successful, the name of the previous bidder
      */
     public String makeBid(Item item, int tryMoney, String name){
-        //assuming that the account was already checked, going for item now
         Item find = findItemInInventory(item);
 //        System.out.println("bidder: "+name+" tryMoney: "
 //                +tryMoney+" bidding on: " + find);
         if(find == null){
             //System.out.println(item.getItemName()+" is not in our inventory");
-            return "NONE"; //item is out of stock
+            //item is out of stock or not in the inventory in the first place
+            return "NONE";
         }else if(tryMoney>find.getCurrentBid()){ //bid is larger
             if(find.getBidder() == null){ //no one has bid
                 //System.out.println("item is now being bid on");
+                //set the bid in place
                 find.setCurrentBid(tryMoney,name);
-                return "START"; //item is now being bid on
-            }else{ //someone has bid
+                //let the caller know that bidding has started on item
+                return "START";
+            }else{ //auction is already taking place on the item
                 //System.out.println("overtaking bid");
+                //save the previous bidder
                 String theOutBid = find.getBidder();
+                //set the new bidder
                 find.setCurrentBid(tryMoney,name);
                 //System.out.println(theOutBid+" has been passed");
-                return theOutBid; //return the outbid for notification
+                //return old bidder, so caller knows who to notify
+                return theOutBid;
             }
-        }else{ //not an overtaking bid
+        }else{
             //System.out.println("Reject bid");
+            //basically: bidder did not offer more money on this attempt
             return "REJECT";
         }
-
-
-
-//    probs going to remove all of this
-//        if(activeAuctions.isEmpty()){
-//            System.out.println("There are no active auctions");
-//            return;
-//        }else if(find == null){
-//            System.out.println("That item is not in our inventory");
-//        }else if(!find.equals(activeAuctions.peekFirst())){
-//            System.out.println("We are not accepting bids for that item " +
-//                    "at this time");
-//        }else{
-//            Bid attempt = new Bid(find, tryMoney, name);
-//            Bid win = getLargerBid(attempt,activeAuctions.peekFirst());
-//            System.out.println("Winner of the battle: "+win);
-//        }
-
     }
 
     public void setBankID(){
@@ -228,38 +147,6 @@ public class AuctionHouse {
         return null;
     }
 
-    private void printActiveAuctions(){ //may not need
-        System.out.println("Active Auctions");
-        for(Bid bid: activeAuctions){
-            System.out.println(bid);
-        }
-    }
-
-    /*
-     * bid a and bid b are assumed be of the same item
-     * @param a one bid
-     * @param b another bid
-     */
-    private Bid getLargerBid(Bid a, Bid b) { //good to go
-        if (a.getBidAmount() == b.getBidAmount()) {
-            return null;
-        }
-        else if(a.getBidAmount() > b.getBidAmount()){
-            return a;
-        }else{
-            return b;
-        }
-    }
-
-    private void printHelp(){ //acutally,might not need this at all, at least, not here
-        System.out.println("How to use your auction house:");
-        System.out.println("1. set up a bank account");
-        System.out.println("use 'bank' command to do this.");
-        System.out.println("2. sell your items.");
-        System.out.println("wait for clients to do this");
-        System.out.println("3. clean exit, type 'quit'.");
-    }
-
     /*
      * reads in the number of items an auction house wishes to sell
      * @param scanner, to read in the number of items
@@ -270,16 +157,21 @@ public class AuctionHouse {
         String command = commandLine.nextLine();
         try{
             int getItems = Integer.parseInt(command);
-            numberOfItems = getItems;
+            if(getItems< 0){
+                System.out.println("That is a negative number");
+            }else if(getItems==0){
+                System.out.println("...That is zero.");
+            }else{
+                numberOfItems = getItems;
+            }
         }catch (NumberFormatException e){
             if(!command.equals("d")) {
                 System.out.println("That is not a number");
             }
         }
         if(defaultItemNum == numberOfItems){
-            System.out.println("using default");
+            System.out.println("Using default");
         }
-        //commandLine.close();
     }
 
 
@@ -288,56 +180,35 @@ public class AuctionHouse {
      * don't have to do all of it by hand...also for fun.
      * @return true if successful, false if file isn't found
      */
-    private boolean readItemList(String fileName){
-        LinkedList<Item> items = new LinkedList<>();
-        int idnum =1;
+    private boolean readItemList(String fileName){ //leaving for encapsulation
+        LinkedList<Item> items = new LinkedList<>(); //a temporary list
+        int idNum =1;
         try{
             Scanner readin = new Scanner(new FileReader(fileName));
-            //Note adds ALL things in text file to auction house  itemlist
+            //adds all items in read in to a temporary list
             while(readin.hasNextLine()){
                 String input = readin.nextLine();
+                //randomizing the prices, because why not?
                 int price = rand.nextInt(maxMinBid);
                 if(price == 0){
                     price++; //add one just so the price isn't zero, ever
                 }
-                Item item = new Item(idnum,input,price); //set max bid at top.
+                //IDs end up consistent across all auction houses
+                Item item = new Item(idNum,input,price); //set max bid at top.
                 items.add(item);
-                idnum++;
+                idNum++;
             }
             readin.close();
         }catch (FileNotFoundException ex){
             System.out.println("file not found");
             return false;
         }
-        //need to randomly pick a certain number of items
-        //I think I will keep the very random id numbers...i feel like it fits
+        //pick a specified number of items, but the items are picked randomly
         while(numberOfItems>0 && !items.isEmpty()){
             int next = rand.nextInt(items.size());
             inventory.add(items.remove(next));
             numberOfItems--;
         }
-
         return true;
     }
-
-    private void printItemList(){
-        System.out.println("Items Available: ");
-        if(!inventory.isEmpty()) {
-            for (Item i : inventory) {
-                System.out.println(i);
-            }
-        }else{
-            System.out.println("nothing");
-        }
-    }
-
-    /*
-    if you close the input stream in one place, it closes everywhere
-    for now, im using a static scanner
-    if it becomes an issue, then I can go back to makeing new ones
-    but I can't close any until the input stream is finished in
-    everything(therefore, at the end of main
-    I've kept hte read in in read Item list, because it isn;t from keyboard
-    sout -systemoutprintln
-     */
 }
