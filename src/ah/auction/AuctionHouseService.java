f8a6a33c7899e372b6864f9b8ae72169a463c65a
samiddaugh <smiddaugh@unm.edu>
2018-12-07 11:34:18 -0700

    trying to make bidders

--- src/ah/auction/AuctionHouseService.java
+++ src/ah/auction/AuctionHouseService.java
@@ -7,8 +7,12 @@ import java.io.*;
 import java.net.Socket;
 import java.util.LinkedList;
 import java.util.Scanner;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class AuctionHouseService implements Runnable {
+    private ExecutorService pool = Executors.newCachedThreadPool();
+    private LinkedList<Bidder> bidders = new LinkedList();
 
     private BankProxy bank;
     //private LinkedList<Item> itemList = new LinkedList<>();
@@ -29,12 +33,57 @@ public class AuctionHouseService implements Runnable {
     public void addNewClient(Socket s) throws IOException {
         PrintWriter out = new PrintWriter(s.getOutputStream(), true);
         BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
-        while(true) {
-            if(in.readLine() != null)
-                System.out.println("AHService received message: " + in.readLine());
-        }
+
+        /* only agents are connecting to the auction house
+         */
+        String name = in.readLine();
+        //create a bidder
+        Bidder newbidder =  new Bidder(name,out,in);
+        bidders.add(newbidder);
+        pool.execute(newbidder);
+
+//        while(true) {
+//            if(in.readLine() != null)
+//                System.out.println("AHService received message: " + in.readLine());
+//        }
     }
 
 
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/ah/auction/Bidder.java
+++ src/ah/auction/Bidder.java
@@ -1,4 +1,36 @@
 package ah.auction;
 
-public class Bidder {
+import java.io.BufferedReader;
+import java.io.PrintWriter;
+
+public class Bidder implements Runnable{
+    private String name;
+    boolean loggedIn = true;
+    private PrintWriter out;
+    private BufferedReader in;
+
+    public Bidder(){
+
+    }
+
+    public Bidder(String callMe, PrintWriter out, BufferedReader in){
+        this.name = callMe;
+        System.out.println("new bidder created! "+name);
+    }
+
+    @Override
+    public void run(){
+        String input;
+        while(loggedIn){
+            try{
+                input = in.readLine();
+                if(input!= null){
+                    System.out.println("Request received from account " + name+ ": " + input);
+                }
+            }catch(Exception e){
+                e.printStackTrace();
+            }
+        }
+    }
+
 }
