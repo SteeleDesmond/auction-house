1d9718123776202ea99712d043beed34543e2e9d
Savanna Middaugh <smiddaugh@unm.edu>
2018-12-07 20:13:09 -0700

    add comments to auction house service

--- src/ah/auction/AuctionHouseService.java
+++ src/ah/auction/AuctionHouseService.java
@@ -8,6 +8,10 @@ import java.util.Scanner;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
+/**
+ * AuctionHouseService is the meat of the program
+ * it runs the auction house it makes
+ */
 public class AuctionHouseService implements Runnable {
     private ExecutorService pool = Executors.newCachedThreadPool();
     private LinkedList<Bidder> bidders = new LinkedList<>();
@@ -16,6 +20,10 @@ public class AuctionHouseService implements Runnable {
     private BankProxy bank;
     private Scanner commandLine;
 
+    /**
+     * Constructor for the service, sets everything up
+     * @param bank takes a bank proxy
+     */
     public AuctionHouseService(BankProxy bank) {
         this.bank = bank;
         commandLine = new Scanner(System.in);
@@ -23,11 +31,22 @@ public class AuctionHouseService implements Runnable {
 
     }
 
+    /**
+     * run, it exists for when we want to add command line gui after
+     * the creation of the whole auction house
+     */
     @Override
     public void run() {
 
     }
 
+    /**
+     * Adds new clients for the auction house
+     * will start a new bidder thread for communication,
+     * then go back to listening for more connections
+     * @param s a socket
+     * @throws IOException if it occurs
+     */
     public void addNewClient(Socket s) throws IOException {
         PrintWriter out = new PrintWriter(s.getOutputStream(), true);
         BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
@@ -38,6 +57,10 @@ public class AuctionHouseService implements Runnable {
         pool.execute(newbidder);
     }
 
+    /**
+     * getter for the inventory
+     * @return the inventory in linked list form
+     */
     public LinkedList<String> getInventory(){
         return ah.getInventoryList();
     }
@@ -61,6 +84,11 @@ public class AuctionHouseService implements Runnable {
         return bank.unblockFunds(bank.getAHKey(), agentBiddingKey, amount);
     }
 
+    /*
+     * finds the bidder based on the key you have
+     * @param bidderkey that you have
+     * @return the bidder, if one is found
+     */
     private Bidder findBidder(String bidderkey) {
         for(Bidder b: bidders) {
             if (b.matches(bidderkey)) {
@@ -69,12 +97,29 @@ public class AuctionHouseService implements Runnable {
         }
         return null;
     }
-    public void createBidThread(Item item, Bidder bidder){
+
+    /*
+     * creates for a bid thread
+     * @param item you want to bid on
+     * @param bidder that is bidding
+     */
+    private void createBidThread(Item item, Bidder bidder){
         Bid bid = new Bid(item,this,bidder);
         bids.add(bid);
         bid.start();
     }
 
+    /**
+     * attempts to make a bid,
+     * it finds the bidder, checks their account, and, if they have the money
+     * will then send their bid attempt along
+     * they will then receive notification based on success
+     * @param bidderKey the string rep of the bidder you are
+     * @param itemName name of the item you want
+     * @param amount of money you want to try spending on it
+     * @return false if bid fails, true if it does not
+     * @throws IOException
+     */
     public boolean attemptToBid(String bidderKey, String itemName, int amount)
     throws IOException{
         System.out.println("Attempting to bid");
@@ -106,6 +151,13 @@ public class AuctionHouseService implements Runnable {
         }
         return true;
     }
+
+    /**
+     * checks to see if you have a winning bid
+     * @param winCard string representation of the bid
+     * @param item that you had bid on
+     * @return true if the current bid information matches, false if it does not
+     */
     public boolean checkWin(String winCard, Item item){
         LinkedList<Item> inventory = ah.getInventory();
         if(inventory.contains(item)){
@@ -120,39 +172,3 @@ public class AuctionHouseService implements Runnable {
         return false;
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
