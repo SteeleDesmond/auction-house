b8b1c576c5b5d2d8c652fb7adab961f4191988db
Savanna Middaugh <smiddaugh@unm.edu>
2018-12-07 20:02:32 -0700

    added comments to bidder

--- src/ah/auction/Bidder.java
+++ src/ah/auction/Bidder.java
@@ -10,6 +10,10 @@ import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.LinkedList;
 
+/**
+ * Bidder functions as a communication thread
+ * on the auction house side between the agent and auction house
+ */
 public class Bidder implements Runnable {
     private String name;
     boolean loggedIn = true;
@@ -19,7 +23,18 @@ public class Bidder implements Runnable {
     private String biddingKey;
     private ArrayList<String> itemsWon = new ArrayList<>();
 
-    public Bidder(String callMe, PrintWriter out, BufferedReader in, AuctionHouseService house) throws IOException {
+    /**
+     * constructor for the Bidder
+     * @param callMe this supposed to be the agents name. it is not
+     * @param out this is the output stream
+     * @param in this is the input stream
+     * @param house this is a refererance to the parent class,
+     *              that class deals with the "real" auction house
+     * @throws IOException if it occurs
+     */
+    public Bidder(String callMe,
+                  PrintWriter out, BufferedReader in, AuctionHouseService house)
+            throws IOException {
         this.name = callMe;
         this.in=in;
         this.out = out;
@@ -28,32 +43,36 @@ public class Bidder implements Runnable {
         System.out.println("New bidder added! Bidding key = " + biddingKey);
     }
 
+    /**
+     * This is the run method
+     * while the bidder is 'logged on'
+     * it will be listened to
+     */
     @Override
     public void run() {
         String input;
         while(loggedIn){
             //System.out.println("Loop");
-            try{
+            try {
                 input = in.readLine();
-                if(input!= null){
-                    System.out.println("Request received from account " + name+ ": " + input);
+                if (input != null) {
+                    System.out.println("Request received from account " + name + ": " + input);
                     AuctionHouseMessages msg = AuctionHouseMessages.valueOf(input);
-                    switch(msg){
+                    switch (msg) {
                         case GETAUCTIONS:
                             sendSuccess();
                             LinkedList<String> inventory = house.getInventory();
                             out.println(inventory.size());
-                            for(String item: inventory){
+                            for (String item : inventory) {
                                 out.println(item);
                             }
                             break;
                         case BID: {
                             String itemName = in.readLine(); // Get the item to bid on
                             int amount = Integer.valueOf(in.readLine());
-                            if(house.attemptToBid(biddingKey, itemName, amount)) {
+                            if (house.attemptToBid(biddingKey, itemName, amount)) {
                                 out.println(AuctionHouseMessages.SUCCESS);
-                            }
-                            else {
+                            } else {
                                 out.println(AuctionHouseMessages.FAILURE);
                             }
                             break;
@@ -61,19 +80,13 @@ public class Bidder implements Runnable {
                         case WON: {
                             out.println(AuctionHouseMessages.SUCCESS);
                             out.println(itemsWon.size());
-                            for(String s : itemsWon) {
+                            for (String s : itemsWon) {
                                 out.println(s);
                             }
                             break;
                         }
-//                        case BID:
-//                            input = in.readLine();
-//                            int money = Integer.parseInt(in.readLine());
-//                            house.makeBid(name,input,money);
-//                            break;
+
                     }
-
-
                 }
             }catch(Exception e){
                 e.printStackTrace();
@@ -81,22 +94,42 @@ public class Bidder implements Runnable {
         }
     }
 
+    /*
+     * method is meant to be a way to notify bidder of success
+     */
     private void sendSuccess() {
         out.println(BankMessages.SUCCESS);
     }
 
+    /**
+     * getter for the items a bidder has won
+     * @return a list of items
+     */
     public ArrayList<String> getItemsWon() {
         return itemsWon;
     }
 
+    /**
+     * Notify the bidder that they have won the item
+     * add it to their winnings
+     * @param item in question
+     */
     public void sendWinNotification(Item item){
         out.println(AuctionHouseMessages.WON);
         addWonItem(item.getItemName());
     }
+
+    /**
+     * Notify the bidder that their bid has been passed
+     * need to find a way to unblock their fundss...
+     */
     public void sendPassNotification(){
         out.println(AuctionHouseMessages.PASS);
     }
 
+    /**
+     * Notify a bidder that their bid has been rejected
+     */
     public void sendRejectNotification(){
         out.println(AuctionHouseMessages.REJECT);
     }
@@ -108,6 +141,11 @@ public class Bidder implements Runnable {
         itemsWon.add(item);
     }
 
+    /**
+     * A method to check to see if you are the bidder being searched for
+     * @param name string of the key that wants to be matched
+     * @return true if this bidders 'name' matches, false if it does not
+     */
     public boolean matches(String name){
         return this.name.equals(name);
     }
